d5a18ec00a42efeb4c2295c80f8cec4c5e156f23
Alexandre Leclerc <66482588+aleclerc7@users.noreply.github.com>
2023-06-06 19:20:12 -0400

    Update CloudflareSynologyDDNSProvider.py
    Added basic instructions to install it in Synology DSM using a cron job, so it works even after an update.

--- CloudflareSynologyDDNSProvider.py
+++ CloudflareSynologyDDNSProvider.py
@@ -1,4 +1,25 @@
 # Original script from: https://github.com/namukcom/SynologyCloudFlareDDNS/blob/master/setddns.py
+# Instructions for installation:
+#   Installation - Simple way (requires DSM 7.0+ or Python3 installed)
+#   1. Open Task Scheduler (Control Panel - [Services] Task Scheduler)
+#   2. Create a user-defined script item. Create - Triggered Task - User-defined script
+#      [General Tab]
+#      Task: Cloudflare DDNS (not important)
+#      User: root
+#      Event: Boot-up
+#      Pre-task: none
+#      Enabled: Checked
+#      
+#      [Task Settings Tab]
+#      [Run Command] User-defined script
+#          curl https://raw.githubusercontent.com/namukcom/SynologyCloudflareDDNS/master/setddns.py | python3 -
+#   3. Press OK
+#   4. Right-Click on the task you've just created.
+#   5. Click Run
+#   6. You can see Cloudflare DDNS has been added to your DDNS list.
+#   7. Setup DDNS in Synology DSM (You can use "API Tokens" or "Global API Key")
+#      See https://github.com/namukcom/SynologyCloudFlareDDNS for more details.
+
 import configparser
 import urllib.request
 import os, stat
